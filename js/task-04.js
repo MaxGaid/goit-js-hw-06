bebe7ed4f08ca739232ebd66910d24fa6c98c835
Max <maxgaydaenko@gmail.com>
2022-07-28 19:54:36 +0300

    4

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+const decrementEl = document.querySelector('button[data-action="decrement"]');
+
+const incrementEl = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+
+let counterValue = 0;
+
+function handleBtnClickPlus() {
+  counterValue = valueEl.textContent;
+  valueEl.textContent = parseInt(counterValue) + 1;
+}
+
+function handleBtnClickMinus() {
+  counterValue = valueEl.textContent;
+  valueEl.textContent = parseInt(counterValue) - 1;
+}
+decrementEl.addEventListener("click", handleBtnClickMinus);
+
+incrementEl.addEventListener("click", handleBtnClickPlus);
+
+// incrementEl.addEventListener('click', () => {
+//     const actionEl = this.dataset.action;
+//     const valueEl = document.querySelector('#value');
+//     const currentValue = 0;
+//     let counterValue = currentValue - 1;
+//     return counterValue = valueEl.value;
+// })
